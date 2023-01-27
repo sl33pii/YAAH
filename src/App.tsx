6fab5f208895a433737a06595ca07dedcbae57a7
BuildTools <zfinkelberg@gmail.com>
2023-01-27 11:34:05 -0300

    html y css basico del landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ReactDOM from 'react-dom'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import bgPic from "./assets/index-background.jpg"
+import steamLogo from "./assets/steam-logo.png"
+library.add(fab, faSearch)
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="nav-bar">
+          <a className='nav-bar-left' href="#">Y.A.A.H.</a>
+          <div className='nav-bar-right'>
+              <a href="#">ABOUT</a>
+              <a href="#">CONTACT</a>
+              <div className='nav-search-bar'>
+                <input type='text' placeholder='SEARCH GAMES' id='input-search'/>
+                <FontAwesomeIcon className='nav-search-icon' icon="search"/>
+              </div>
+              <div className='nav-profile-img'></div>
+          </div>
+      </nav>
+      <div className='container-title-content'>
+        <div className='title-content-top'>
+          <p className='title'>YET ANOTHER ACHIEVEMENT HELPER</p>
+          <p className='subtitle'>A RESOURCE FOR GAME COMPLETIONISTS</p>
+        </div>
+        <div className='title-content-bottom'>
+          <button className='btn-style-1'>LOG IN WITH STEAM ACCOUNT <img className='steam-logo-img' src={steamLogo}/></button>
+          <div className='container-buttons-lg-su'>
+              <button className='btn-style-2'>LOG IN</button>
+              <button className='btn-style-3'>SIGN UP</button>
+          </div>
+        </div>
+      </div>
+      <div className='container-game-list'>
+          <ul>
+              
+          </ul>
+      </div>
     </div>
   );
 }
